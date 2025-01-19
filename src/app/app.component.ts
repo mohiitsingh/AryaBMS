d79d56fca89855a0fe45065402a6550de2702572
mohiitsingh <ms6065097@gmail.com>
2025-01-20 01:15:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../components/navbar/navbar.component";
+import { HeroComponent } from "../components/hero/hero.component";
+import { AboutComponent } from "../components/about/about.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
